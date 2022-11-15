54857adfc9d3da68ba6394f4cb8476ddf88e8f06
David Quezada <david.quezada.m@gmail.com>
2022-11-15 20:54:29 -0300

    cambios

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/mensaje', (req,res) => {
-    res.send('Hola Node.js desde Heroku!');
+    res.send('MENSAJE NUEVO!');
 })
 
 const PORT = process.env.PORT || 8080
